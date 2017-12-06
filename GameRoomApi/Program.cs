8c6af65f2e52a56e210a028ff294afc319538b90
Kulinich Vlad <kulinich_v@hotmail.com>
2017-12-06 09:03:27 +0200

    Add project files.

---
+++ GameRoomApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRoomApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
