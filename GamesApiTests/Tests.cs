9496248dff9437283107a91e9bb751ea088e373a
Kulinich Vlad <kulinich_v@hotmail.com>
2017-12-08 03:21:20 +0200

    Added very simple tests

---
+++ GamesApiTests/Tests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using GamesAPI.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using static GamesAPI.Actions.GameActions;
+
+namespace GamesApiTests
+{
+    [TestClass]
+    public class Tests
+    {
+        WebSocketServer server;
+        GamesAPI.GamesAPI gamesAPI = new GamesAPI.GamesAPI();
+
+        [TestInitialize]
+        public void StartServer()
+        {
+            server = new WebSocketServer("ws://localhost:8888/");
+            gamesAPI.AttachAPI(server, new GamesAPI.DAO.GameDAO(), "/Games");
+            server.Start();
+        }
+
+        [TestCleanup]
+        public void ServerStop()
+        {
+            server.Stop();
+        }
+
+        [TestMethod]
+        public void TestWebSocketWithServiceIsListening()
+        {
+            Assert.AreEqual(true, server.IsListening);
+        }
+
+        [TestMethod]
+        public void TestGamesUpdate()
+        {
+            var ws = new WebSocket("ws://localhost:8888/Games");
+
+            string getMsg = "";
+            ws.OnMessage += (sender, e) => { getMsg = e.Data; };
+
+            ws.Connect();
+
+            ws.Send(JsonConvert.SerializeObject(new { type = GET_GAMES }));
+
+            Thread.Sleep(1000);
+
+            JToken token = JObject.Parse(getMsg);
+            Assert.AreEqual((string)token.SelectToken("type"), UPDATE_GAMES);
+        }
+
+        [TestMethod]
+        public void TestGameCreate()
+        {
+            var ws = new WebSocket("ws://localhost:8888/Games");
+
+            string getMsg = "";
+            ws.OnMessage += (sender, e) => getMsg = e.Data;
+
+            ws.Connect();
+
+            ws.Send(JsonConvert.SerializeObject(new { type = CREATE_GAME, data = new { Title = "GameTitle"} }));
+
+            Thread.Sleep(1000);
+
+            JToken token = JObject.Parse(getMsg);
+            Assert.AreEqual((string)token.SelectToken("type"), GAME_CREATED);
+        }
+    }
+}
---
+++ UsersTests/Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using static UsersAPI.Actions.UserActions;
+
+namespace UsersTests
+{
+    [TestClass]
+    public class Tests
+    {
+        WebSocketServer server;
+        UsersAPI.UsersAPI usersAPI = new UsersAPI.UsersAPI();
+
+        [TestInitialize]
+        public void StartServer()
+        {
+            server = new WebSocketServer("ws://localhost:8888/");
+            usersAPI.AttachAPI(server, new UsersAPI.DAO.UserDAO(), "/Users");
+            server.Start();
+        }
+
+        [TestCleanup]
+        public void ServerStop()
+        {
+            server.Stop();
+        }
+
+        [TestMethod]
+        public void TestWebSocketWithServiceIsListening()
+        {
+            Assert.AreEqual(true, server.IsListening);
+        }
+
+        [TestMethod]
+        public void TestUsersUpdate()
+        {
+            var ws = new WebSocket("ws://localhost:8888/Users");
+
+            string getMsg = "";
+            ws.OnMessage += (sender, e) => { getMsg = e.Data; };
+
+            ws.Connect();
+
+            Thread.Sleep(1000);
+
+            JToken token = JObject.Parse(getMsg);
+            Assert.AreEqual((string)token.SelectToken("type"), UPDATE_USERS);
+        }
+    }
+}
